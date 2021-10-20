76af113184e9503ec9a8c8ff7055b8097dc98642
MinWoo <mynooo7688@gmail.com>
2021-10-20 23:57:35 +0900

    스택, 백준 10828번

---
+++ test1/day3.py
@@ -0,0 +1,44 @@
+#백준 10828번문제.(스택)
+
+##정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+##명령은 총 다섯 가지이다.
+
+##push X: 정수 X를 스택에 넣는 연산이다.
+##pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+##size: 스택에 들어있는 정수의 개수를 출력한다.
+##empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+##top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+import sys
+
+N = int(sys.stdin.readline())
+stack_list =[]
+
+for i in range():
+    func = list(sys.stdin.readline().split())
+
+    if func[0]=="push":
+        stack_list.append(int(func[1]))
+
+    elif func[0]=="pop":
+        if not stack_list:
+            print(-1)
+
+        else:
+            print(stack_list.pop())
+
+    elif func[0] =="top":
+        if not stack_list:
+            print(-1)
+        else:
+            print(stack_list[-1])
+
+    elif func[0]=="size":
+        print(len(stack_list))
+
+    else:
+        if len(stack_list)==0:
+            print(1)
+        else:
+            print(0)
